989fc30ba851f6bb5c22cff5126a27288dc60353
Vinaykumarthorlakonda <155621613+Vinaykumarthorlakonda@users.noreply.github.com>
2026-05-28 11:32:56 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -26,7 +26,7 @@ function Signup({ setShowSignup }) {
 
         try {
             const res = await axios.post(
-                "http://localhost:8080/auth/register",
+                "https://employee-management-backend-8uip.onrender.com/auth/register/auth/register",
                 user
             );
 
