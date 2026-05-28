e90e08857b1967c70d25cceba0537244cb81df7a
Vinaykumarthorlakonda <155621613+Vinaykumarthorlakonda@users.noreply.github.com>
2026-05-28 11:43:46 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -22,7 +22,7 @@ function Login({ setIsLoggedIn, setShowSignup }) {
 
         try {
             const res = await axios.post(
-                "https://employee-management-backend-8uip.onrender.com/auth/register/auth/login",
+                "https://employee-management-backend-3byo.onrender.com/auth/login",
                 user
             );
 
